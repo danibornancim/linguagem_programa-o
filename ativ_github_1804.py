c25aa5d497b66b7618b637d6ee0d719c3b37572b
danibornancim <130069981+danibornancim@users.noreply.github.com>
2023-04-18 08:12:48 -0300

    Add files via upload
    exercício dia 18/04

---
+++ ativ_github_1804.py
@@ -0,0 +1,8 @@
+class aluno:
+    def __init__ (self, nome, matricula, nota_av1, nota_av2, trabalho):
+        self.nome = nome 
+        self.matricula = matricula
+        self.nota_av1 = nota_av1
+        self.nota_av2 = nota_av2
+        self.trabalho = trabalho
+
---
+++ teste.ativ.py
@@ -0,0 +1,10 @@
+from ativ_github_1804 import aluno
+
+def notas():
+    nome = input('Digite seu nome: ')
+    matricula = int(input('Digite sua matricula: '))
+    nota_av1 = int(input('Digite sua primeira nota: '))
+    nota_av2 = int(input('Digite sua segunda nota: '))
+    trabalho = int(input('Digite a nota do trabalho: '))
+
+notas()
